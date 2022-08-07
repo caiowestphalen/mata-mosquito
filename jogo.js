9fe4cab24cb78a4b1e2fca39c25a97184edfa280
Caio Westphalen <caiowestphalen@hotmail.com>
2022-08-07 07:24:54 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -3,6 +3,8 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 15
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -13,6 +15,20 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function(){
+
+    tempo -= 1
+
+    if(tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = 'vitoria.html'
+    }   else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+   
+    
+}, 1000)
 
 function posicaoRandomica() {
 
@@ -20,6 +36,17 @@ function posicaoRandomica() {
     //remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+            
+            window.location.href = 'fim-de-jogo.html'
+        } else {
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+
+            vidas++
+
+        }
+
     }
 
 	var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -38,6 +65,9 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
 	document.body.appendChild(mosquito)
 }
